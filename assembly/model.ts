20ca20e1f788e491f45c67ab9006642fdc5358eb
Carlos Huerta Garcia <huerta2502@gmail.com>
2022-03-29 20:19:14 -0600

    finished doc and starting models assemblyscript implementation

--- assembly/model.ts
+++ assembly/model.ts
@@ -1,16 +1,80 @@
 /**
  * 
  */
-import { PersistentSet, PersistentUnorderedMap, u128 } from "near-sdk-as";
+import {
+    context, // visibility into account, contract and blockchain details
+    logging, // append to the execution environment log (appears in JS Developer Console when using near-api-js)
+    storage, // key-value store for the contract (used by PersistentMap, PersistentVector and PersistentDeque)
+    PersistentMap, // data structure that wraps storage to appear like a Map
+    PersistentVector, // data structure that wraps storage to appear like a Vector
+    PersistentDeque, // data structure that wraps storage to appear like a Deque
+    PersistentSet, // data structure that wraps storage to appear like a Set
+    ContractPromise, // make asynchronous calls to other contracts and receive callbacks
+    base58, // utility base58 encoder
+    base64, // utility base64 encoder / decoder
+    math, // utility math functions for hashing using SHA and Keccak as well as pseudo-random data
+    utils, // utility type conversion and read_register
+} from "near-sdk-as";
+
 /**
  * 
  */
 @nearBindgen
 export class User {
-    name: string;
-    email: string;
-    password: string;
-    collectives: PersistentSet<string>;
+    #accountId: string;
+    #name: string;
+    #email: string;
+    #password: string;
+    #collectives: PersistentSet<string>;
+
+    constructor( accountId: string, name: string, email: string, password: string) {
+        this.#accountId = accountId;
+        this.#name = name;
+        this.#email = email;
+        this.#password = password;
+        this.#collectives = new PersistentSet<string>(accountId + "_collectives");
+    }
+
+    get accountId(): string {
+        return this.#accountId;
+    }
+
+    set accountId(accountId: string) {
+        this.#accountId = accountId;
+    }
+
+    get name(): string {
+        return this.#name;
+    }
+
+    set name(name: string) {
+        this.#name = name;
+    }
+
+    get email(): string {
+        return this.#email;
+    }
+
+    set email(email: string) {
+        this.#email = email;
+    }
+
+    get password(): string {
+        return this.#password;
+    }
+
+    set password(password: string) {
+        this.#password = password;
+    }
+
+    addCollective(collectiveId: string): void {
+        this.#collectives.add(collectiveId);
+    }
+
+    getCollectives(): Array<string> {
+        return this.#collectives.values();
+    }
+
 }
 
 /**
@@ -18,9 +82,11 @@ export class User {
  */
 @nearBindgen
 export class Collective {
+    collectiveId: string;
     name: string;
     infoLink: string;
     collectives: PersistentSet<string>;
+
 }
 
 /**
@@ -31,3 +97,37 @@ export class Deliberation {
     name: string;
     
 }
+
+/**
+ * 
+ */
+@nearBindgen
+export class Resource {
+    name: string;
+    
+}
+
+/**
+ * 
+ */
+@nearBindgen
+export class Result {
+    name: string;
+    
+}
+
+/**
+ * 
+ */
+@nearBindgen
+export class FollowUp {
+    name: string;
+    
+}
+
+export let userRegistry = ;;
+export let collectiveRegistry = ;;
+export let deliberationRegistry = ;
+export let resultRegistry = ;
+export let followupRegistry = ;
+export let resourceRegistry = ;
